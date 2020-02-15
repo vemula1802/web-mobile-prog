46f35fde3b28241e1d8b65039eca3949b8c4db2c
vemula1802 <51023788+vemula1802@users.noreply.github.com>
2020-02-15 16:00:17 -0600

    Changes

--- ICP4/Source/script.js
+++ ICP4/Source/script.js
@@ -12,27 +12,28 @@ function getGithubInfo(user) {
         showUser(data);
 
     }).fail(function(){
-        console.log("Some error Happened");
+        console.log("Some Error Happened");
         noSuchUser(user);
     });
-
 }
 
 function showUser(user) {
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
-
-    document.getElementById('txtname').innerText=user.name;
-    document.getElementById('txtid').innerText=user.id;
-    document.getElementById('imgavg').src=user.avatar_url;
-    document.getElementById('txturl').href=user.url;
-    document.getElementById('txturl').innerText=user.html_url;
+    document.getElementById('imgAvatar').src=user.avatar_url;
+    document.getElementById('textName').innerText=user.name;
+    document.getElementById('textId').innerText=user.id;
+    document.getElementById('textUrl').href=user.url;
+    document.getElementById('textUrl').innerText=user.html_url;
+    document.getElementById('textRepository').innerText=user.public_repos;
 }
+
 function noSuchUser(username) {
     //3. set the elements such that a suitable message is displayed
     if(data.message == "Not Found" || username == '') {
         alert("User not found");
     }
 }
+
 $(document).ready(function () {
     $(document).on('keypress', '#username', function (e) {
         //check if the enter(i.e return) key is pressed
@@ -42,9 +43,14 @@ $(document).ready(function () {
             //reset the text typed in the input
             $(this).val("");
             //get the user's information and store the respsonse
-            getGithubInfo(username);
+            response = getGithubInfo(username);
             //if the response is successful show the user's details
-
+            if (response.status == 200) {
+                showUser(JSON.parse(response.responseText));
+                //else display suitable message
+            } else {
+                noSuchUser(username);
+            }
         }
     })
 });
